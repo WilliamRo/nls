6c577e845de440131327e037aec30fd61c460bbc
William <willi4m@zju.edu.cn>
2018-02-05 22:26:50 +0800

    [M] applied smart train logic

--- wh_testbed/mlp_script.py
+++ wh_testbed/mlp_script.py
@@ -16,46 +16,41 @@ import wh_model_lib
 WH_PATH = '../data/wiener_hammerstein/whb.tfd'
 VAL_SIZE = 20000
 
-MEMORY_DEPTH = 160
+MEMORY_DEPTH = 80
 D = MEMORY_DEPTH
-NN_EPOCH = 50
+NN_EPOCH = 500
 NN_HID_DIMS = [2 * D] * 4
 MODEL_MARK = 'mlp_D{}_{}_{}'.format(MEMORY_DEPTH, NN_HID_DIMS, 'lrelu')
-FLAGS.epoch_tol = 5
+# FLAGS.epoch_tol = 10
 
 NN_LEARNING_RATE = 0.001
-# NN_LEARNING_RATE = 0.0001
-# NN_LEARNING_RATE = 0.00001
-# NN_LEARNING_RATE = 0.000001
 
-# NN_DEGREE = 4
-# NN_MAX_VOL_ORD = 99
+FLAGS.smart_train = True
 
 FLAGS.train = True
-FLAGS.train = False
+# FLAGS.train = False
 
 FLAGS.overwrite = True
-FLAGS.overwrite = False
+# FLAGS.overwrite = False
 
 FLAGS.save_best = False
-FLAGS.save_best = True
+# FLAGS.save_best = True
 
 # Turn off overwrite while in save best mode
 FLAGS.overwrite = FLAGS.overwrite and not FLAGS.save_best and FLAGS.train
 
 EVALUATION = not FLAGS.train
 PLOT = EVALUATION
-
-model = wh_model_lib.mlp_00(MEMORY_DEPTH, MODEL_MARK, NN_HID_DIMS,
-                         NN_LEARNING_RATE)
 # =============================================================================
-
 # Load data set
 train_set, val_set, test_set = load_wiener_hammerstein(
   WH_PATH, depth=MEMORY_DEPTH)
 assert isinstance(train_set, DataSet)
 assert isinstance(val_set, DataSet)
 assert isinstance(test_set, DataSet)
+
+model = wh_model_lib.mlp_00(
+  MEMORY_DEPTH, MODEL_MARK, NN_HID_DIMS, NN_LEARNING_RATE)
 
 # Define model and identify
 if FLAGS.train: model.identify(
--- wh_testbed/wh_model_lib.py
+++ wh_testbed/wh_model_lib.py
@@ -17,6 +17,7 @@ from models.neural_net import NeuralNet
 # region : MLP
 
 def mlp_00(memory_depth, mark, hidden_dims, learning_rate=0.001):
+  strength = 0
   # Initiate a predictor
   model = NeuralNet(memory_depth, mark=mark)
   nn = model.nn
@@ -24,8 +25,8 @@ def mlp_00(memory_depth, mark, hidden_dims, learning_rate=0.001):
 
   # Add layers
   nn.add(Input([memory_depth]))
-  _add_fc_relu_layers(nn, hidden_dims, 'lrelu')
-  nn.add(Linear(output_dim=1))
+  _add_fc_relu_layers(nn, hidden_dims, 'lrelu', strength=strength)
+  nn.add(Linear(output_dim=1, weight_regularizer='l2', strength=strength))
 
   # Build model
   nn.build(loss='euclid', metric='rms_ratio', metric_name='RMS(err)%',
@@ -38,12 +39,12 @@ def mlp_00(memory_depth, mark, hidden_dims, learning_rate=0.001):
 
 # region : Layers
 
-def _add_fc_relu_layers(nn, hidden_dims, activation='lrelu'):
+def _add_fc_relu_layers(nn, hidden_dims, activation='lrelu', strength=0.0):
   assert isinstance(nn, Predictor)
   assert isinstance(hidden_dims, (tuple, list))
 
   for dim in hidden_dims:
-    nn.add(Linear(output_dim=dim))
+    nn.add(Linear(output_dim=dim, weight_regularizer='l2', strength=strength))
     nn.add(Activation(activation))
 
 # endregion : Utilities
