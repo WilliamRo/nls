4ca9157ceaf0605d6f8019f00c12b983f5f6aa62
William <willi4m@zju.edu.cn>
2018-03-06 15:43:48 +0800

    ...

---
+++ wh_testbed/hall_of_fame_script.py
@@ -0,0 +1,54 @@
+from tframe import FLAGS
+from tframe import console
+
+from signals.utils.dataset import load_wiener_hammerstein, DataSet
+
+import wh_model_lib
+
+# =============================================================================
+# Global configuration
+WH_PATH = '../data/wiener_hammerstein/whb.tfd'
+VAL_SIZE = 20000
+
+MEMORY_DEPTH = 80
+D = MEMORY_DEPTH
+NN_EPOCH = 500
+NN_HID_DIMS = [D] * 10
+NN_LEARNING_RATE = 0.001
+BATCH_SIZE = 32
+PRINT_CYCLE = 10
+
+FLAGS.train = True
+# FLAGS.train = False
+FLAGS.overwrite = True
+# FLAGS.overwrite = False
+FLAGS.save_best = False
+# FLAGS.save_best = True
+
+FLAGS.smart_train = True
+FLAGS.epoch_tol = 20
+
+# Turn off overwrite while in save best mode
+FLAGS.overwrite = FLAGS.overwrite and not FLAGS.save_best and FLAGS.train
+
+EVALUATION = not FLAGS.train
+PLOT = EVALUATION
+# =============================================================================
+# Load data set
+train_set, val_set, test_set = load_wiener_hammerstein(
+  WH_PATH, depth=MEMORY_DEPTH)
+assert isinstance(train_set, DataSet)
+assert isinstance(val_set, DataSet)
+assert isinstance(test_set, DataSet)
+
+model = wh_model_lib.mlp_00(MEMORY_DEPTH, NN_HID_DIMS, NN_LEARNING_RATE)
+
+# Define model and identify
+if FLAGS.train: model.identify(
+  train_set, val_set, batch_size=BATCH_SIZE,
+  print_cycle=PRINT_CYCLE, epoch=NN_EPOCH)
+
+# Evaluation
+if EVALUATION:
+  model.evaluate(train_set, start_at=1000, plot=False)
+  model.evaluate(test_set, start_at=1000, plot=PLOT)
