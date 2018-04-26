4920cc5cd7ece2fc77d91d7fc2cb786f044da628
William <willi4m@zju.edu.cn>
2018-04-26 21:31:52 +0800

    ...

--- wh_gcp/trainer/model_lib.py
+++ wh_gcp/trainer/model_lib.py
@@ -3,6 +3,8 @@ from tframe.layers import Activation
 from tframe.layers import Linear
 from tframe.layers import Input
 
+from tframe.nets.resnet import ResidualNet
+
 from models.neural_net import NeuralNet
 
 
@@ -11,7 +13,7 @@ from models.neural_net import NeuralNet
 def mlp_00(mark, memory_depth, hidden_dim, hidden_num, learning_rate,
            activation='relu'):
   # Initiate a predictor
-  model = NeuralNet(memory_depth, mark=mark)
+  model = NeuralNet(memory_depth, mark=mark, nn_class=Predictor)
   nn = model.nn
   assert isinstance(nn, Predictor)
 
@@ -29,3 +31,31 @@ def mlp_00(mark, memory_depth, hidden_dim, hidden_num, learning_rate,
   return model
 
 # endregion : MLP
+
+# region : ResNet
+
+def res_00(mark, memory_depth, hidden_dim, num_blocks, learning_rate,
+           activation='relu'):
+  model = NeuralNet(memory_depth, mark=mark, nn_class=Predictor)
+  nn = model.nn
+  assert isinstance(nn, Predictor)
+
+  # Add blocks
+  nn.add(Input([memory_depth]))
+  nn.add(Linear(output_dim=hidden_dim))
+  nn.add(Activation(activation))
+  def add_res_block():
+    net = nn.add(ResidualNet())
+    net.add(Linear(output_dim=hidden_dim))
+    net.add(Activation(activation))
+    net.add_shortcut()
+  for _ in range(num_blocks): add_res_block()
+  nn.add(Linear(output_dim=1))
+
+  # Build model
+  model.default_build(learning_rate)
+
+  # Return model
+  return model
+
+# endregion : ResNet
---
+++ wh_gcp/trainer/res_task.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+
+try:
+  from tframe import hub
+  from tframe import console
+  from tframe import SaveMode
+  from tframe.trainers import SmartTrainer, SmartTrainerHub
+
+  from models.neural_net import NlsHub
+  from signals.utils.dataset import load_wiener_hammerstein, DataSet
+  import trainer.model_lib as model_lib
+  hub.data_dir = '../../data/wiener_hammerstein/whb.tfd'
+except:
+  import sys, os
+  abspath = os.path.abspath(__file__)
+  dn = os.path.dirname
+  nls_root = dn(dn(dn(abspath)))
+  sys.path.insert(0, nls_root)   # nls
+  sys.path.insert(0, dn(dn(abspath)))       # wh_gcp
+  sys.path.append(dn)
+  del dn
+
+  from tframe import hub
+  from tframe import console
+  from tframe import SaveMode
+  from tframe.trainers import SmartTrainer, SmartTrainerHub
+
+  from models.neural_net import NlsHub
+  from signals.utils.dataset import load_wiener_hammerstein, DataSet
+  import trainer.model_lib as model_lib
+
+  hub.data_dir = os.path.join(nls_root, 'data/wiener_hammerstein/whb.tfd')
+
+
+def main(_):
+  console.start('ResNet task')
+
+  # Configurations
+  th = NlsHub(as_global=True)
+  th.memory_depth = 80
+  th.num_blocks = 1
+  th.multiplier = 4
+  th.hidden_dim = th.memory_depth * th.multiplier
+
+  th.mark = 'res_h{}_0'.format(th.num_blocks + 1)
+  th.epoch = 50000
+  th.batch_size = 64
+  th.learning_rate = 0.0001
+  th.validation_per_round = 30
+
+  th.train = True
+  th.smart_train = True
+  th.idle_tol = 20
+  th.max_bad_apples = 4
+  th.lr_decay = 0.6
+  th.early_stop = True
+  th.save_mode = SaveMode.ON_RECORD
+  th.warm_up_rounds = 50
+  th.overwrite = True
+  th.export_note = True
+  th.summary = False
+  th.save_model = True
+
+  # Get model
+  model = model_lib.res_00(th.mark, th.memory_depth, th.hidden_dim,
+                           th.num_blocks, th.learning_rate)
+  # Load data
+  train_set, val_set, test_set = load_wiener_hammerstein(
+    th.data_dir, depth=th.memory_depth)
+  assert isinstance(train_set, DataSet)
+  assert isinstance(val_set, DataSet)
+  assert isinstance(test_set, DataSet)
+
+  # Train or evaluate
+  if th.train:
+    model.nn.train(train_set, validation_set=val_set, trainer_hub=th)
+  else:
+    model.evaluate(train_set, start_at=th.memory_depth)
+    model.evaluate(val_set, start_at=th.memory_depth)
+    model.evaluate(test_set, start_at=th.memory_depth)
+
+  # End
+  console.end()
+
+
+if __name__ == '__main__':
+  tf.app.run()
+
