5665e289385de4d28a607aa8d0da5978f3330eac
William <willi4m@zju.edu.cn>
2018-05-06 16:54:45 +0800

    hello monitor

--- wh_gcp/trainer/mlp_task.py
+++ wh_gcp/trainer/mlp_task.py
@@ -35,24 +35,23 @@ except:
 def main(_):
   console.start('mlp task')
 
-  description = '0'
+  description = 'm'
   # Configurations
   th = NlsHub(as_global=True)
-  th.memory_depth = 80
-  th.num_blocks = 1
-  th.multiplier = 8
+  th.memory_depth = 40
+  th.num_blocks = 2
+  th.multiplier = 2
   th.hidden_dim = th.memory_depth * th.multiplier
 
   th.mark = 'mlp-{}x({}x{})-{}'.format(
     th.num_blocks, th.memory_depth, th.multiplier, description)
   th.epoch = 50000
   th.batch_size = 64
-  th.learning_rate = 0.0001
-  # th.reg_strength = 0.0001
-  th.validation_per_round = 30
+  th.learning_rate = 0.001
+  th.validation_per_round = 20
 
   th.train = True
-  th.smart_train = True
+  th.smart_train = False
   th.idle_tol = 20
   th.max_bad_apples = 4
   th.lr_decay = 0.6
@@ -61,8 +60,12 @@ def main(_):
   th.warm_up_rounds = 50
   th.overwrite = True
   th.export_note = True
-  th.summary = False
-  th.save_model = True
+  th.summary = True
+  th.monitor = True
+  th.save_model = False
+
+  th.allow_growth = False
+  th.gpu_memory_fraction = 0.4
 
   # Get model
   model = model_lib.mlp_00(th)
@@ -77,9 +80,10 @@ def main(_):
   if th.train:
     model.nn.train(train_set, validation_set=val_set, trainer_hub=th)
   else:
+    console.show_status('Evaluating ...')
     model.evaluate(train_set, start_at=th.memory_depth)
     model.evaluate(val_set, start_at=th.memory_depth)
-    model.evaluate(test_set, start_at=th.memory_depth)
+    model.evaluate(test_set, start_at=th.memory_depth, plot=True)
 
   # End
   console.end()
